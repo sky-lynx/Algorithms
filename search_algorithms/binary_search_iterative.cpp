34c6ca8dc857d66cc14f685a387c940a96320855
Yash Kumar Singh <yash.singh.k08@gmail.com>
2018-07-17 07:19:57 +0530

    added binary_search_iterative.cpp, jump_search.cpp

--- search_algorithms/binary_search_iterative.cpp
+++ search_algorithms/binary_search_iterative.cpp
@@ -3,4 +3,31 @@
 
 using namespace std;
 
-int searcher(int* arr, int )
+int searcher(int* arr, int key, int l, int r)
+{
+  int m;
+
+  while(l != r)
+  {
+    m = (l + r) / 2;
+    if(arr[m] == key)
+      return m;
+    else if(arr[m] > key)
+      r = m-1;
+    else
+      l = m+1;
+  }
+  return -1;
+}
+
+int main(void)
+{
+  int array[] = {1,2,3,4,5,6,7};
+  int number = 8;
+
+  int lower = 0;
+  int upper = sizeof(array) / sizeof(*array);
+
+  cout<<searcher(array, number, lower, upper)<<'\n';
+  return 0;
+}
---
+++ search_algorithms/jump_search.cpp
@@ -0,0 +1,42 @@
+// retunr position index, else returns -1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int seacher(int* arr, int key, int n)
+{
+  int step = sqrt(n);
+  int prev;
+
+  while(arr[min(step, n) - 1] < key)
+  {
+    prev = step;
+    step += sqrt(n);
+    if(prev >= n)
+      return -1;
+  }
+
+  while(arr[prev] < key)
+  {
+    prev++;
+    if(prev == min(step, n))
+      return -1;
+  }
+
+  if(arr[prev] == key)
+    return prev;
+
+  return -1;
+}
+
+int main(void)
+{
+  int array[] = {1,2,3,4,5,6,7};
+  int number = 7;
+  int size = sizeof(array) / sizeof(*array);
+
+  cout<<seacher(array, number, size)<<'\n';
+
+  return 0;
+}
